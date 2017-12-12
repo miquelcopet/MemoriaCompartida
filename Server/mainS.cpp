c512c4f0a0d1a1570b98ffdeb444361281a51373
miquelcopet <16miquel11@gmail.com>
2017-12-13 00:50:07 +0100

    Multiplatform Server update

--- Server/mainS.cpp
+++ Server/mainS.cpp
@@ -34,7 +34,10 @@ string to_String(int i)
     return ss.str();
 }
 
-int crearSegmento (int id_memoria, int key, int BUFFER_SIZE) {
+#ifdef OS_WINDOWS
+
+#else
+	int crearSegmento (int id_memoria, int key, int BUFFER_SIZE) {
     #ifdef OS_WINDOWS
 
     #else
@@ -45,34 +48,69 @@ int crearSegmento (int id_memoria, int key, int BUFFER_SIZE) {
 
     #endif
     return id_memoria;
-}
+	}
 
-void * mapearMemoria (void* pBuf, int id_memoria) {
-    #ifdef OS_WINDOWS
+	void * mapearMemoria (void* pBuf, int id_memoria) {
+		#ifdef OS_WINDOWS
 
-    #else
-        pBuf = shmat (id_memoria, (char *)0, 0);
-        if (pBuf == NULL) {
-            cout << "ERROR: Reservar la memoria" << endl;
-        }
-    #endif
-    return pBuf;
-}
+		#else
+			pBuf = shmat (id_memoria, (char *)0, 0);
+			if (pBuf == NULL) {
+				cout << "ERROR: Reservar la memoria" << endl;
+			}
+		#endif
+		return pBuf;
+	}
+#endif
+
 
 int main() {
+	
+	#ifdef OS_WINDOWS
+		string sharedName = "Global\\MyFileMappingObject";
+		string message = "";
+		HANDLE hMapFile;
 
-    //Creamos una clave para identificar el espacio de memória
-    int key = 7680;
-    int BUFFER_SIZE = sizeof(Mensaje) * 2;
-    int id_memoria = 0;
-    void * pBuf;
-    string message="";
+		hMapFile = CreateFileMapping(
+					 INVALID_HANDLE_VALUE,    // use paging file
+					 NULL,                    // default security
+					 PAGE_READWRITE,          // read/write access
+					 0,                       // maximum object size (high-order DWORD)
+					 BUF_SIZE,                // maximum object size (low-order DWORD)
+					 sharedName.c_str());     // name of mapping object
 
-    //Creamos el segmento de memória
-    id_memoria = crearSegmento(id_memoria, key, BUFFER_SIZE);
+		if (hMapFile == NULL)
+		{
+		cout << "No se ha podido crear el segmento de memoria (" << GetLastError() << ").\n";
+		return 1;
+		} // end if
 
-    //Se mapea la memoria
-    pBuf = mapearMemoria(pBuf, id_memoria);
+		void *pBuf = (void *) MapViewOfFile(hMapFile,    // handle to map object
+									FILE_MAP_ALL_ACCESS, // read/write permission
+										0,
+									0,
+										BUF_SIZE);
+
+		if (pBuf == NULL) {
+		cout << "No se ha podido completar el mapeo (" << GetLastError() << ").\n";
+		CloseHandle(hMapFile);
+		return 1;
+		} // end if
+	#else
+	    //Creamos una clave para identificar el espacio de memória
+		int key = 7680;
+		int BUFFER_SIZE = sizeof(Mensaje) * 2;
+		int id_memoria = 0;
+		void * pBuf;
+		string message="";	
+	
+		//Creamos el segmento de memória
+		id_memoria = crearSegmento(id_memoria, key, BUFFER_SIZE);
+
+		//Se mapea la memoria
+		pBuf = mapearMemoria(pBuf, id_memoria);
+	#endif
+
 
     cout << ".:SERVIDOR:." << endl;
     cout << "Introduzca q y apriete ENTER para salir del programa" << endl;
@@ -80,21 +118,23 @@ int main() {
     Mensaje *messageServer = (Mensaje *) pBuf + 0;
     Mensaje const *messageClient = ((Mensaje *) pBuf) + 1;
 
-    /// Sets new messsage (and message id)
+    ///Crea el nuevo mensaje con su ID correspondiente
     messageServer->id = messageServer->id + 1;
     message = string("Hola Caracola") + to_String(messageServer->id);
     cout << "Preparando el mensaje numero #" << messageServer->id <<  "." << endl;
     memcpy(&messageServer->message, message.c_str(), strlen(message.c_str()) + 1);
 
-    /// Wait until message is received.
+    ///Espera hasta recibir el mensaje
     bool salir = false;
     string choice;
     cin >> choice;
+	
+	//Sale del bucle si el usuario introduce una "q"
     do {
 	if (messageClient->ack == messageServer->id) {
 	    cout << "message #" << messageServer->id << " received by client." << endl;
 
-	    /// Sets next message:
+	    ///Prepara el proximo mensaje
 	    messageServer->id = messageServer->id + 1;
 	    message = string("hola caracola (") + to_string(messageServer->id) + string(").");
 	    cout << "Setting message #" << messageServer->id <<  "." << endl;
@@ -110,9 +150,13 @@ int main() {
 
     } while(salir == false);
 
-    //LIbera la memória compartida
-    shmdt ((char *)pBuf);
-    shmctl (id_memoria, IPC_RMID, (struct shmid_ds *)NULL);
+	#ifdef OS_WINDOWS
+		CloseHandle (hMapFile);
+	#else
+		shmdt((char *)pBuf);
+		shmctl (id_memoria, IPC_RMID, (struct shmid_ds *)NULL);
+	#endif
+
 
     return 0;
 }
